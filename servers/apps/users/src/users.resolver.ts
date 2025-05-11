832f0fd679e8c94060dfbf5ed4891efe868d9176
Suyash Pandey <suyashpandey607@gmail.com>
2025-05-11 23:05:25 +0530

    feat: implement user registration and retrieval with Prisma integration; nestjs, graphql setup complete

--- servers/apps/users/src/users.resolver.ts
+++ servers/apps/users/src/users.resolver.ts
@@ -1,37 +1,33 @@
 // import { UseFilters } from "@nestjs/common";
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
-import { UsersService } from "./users.service";
-import { RegisterResponse } from "./types/user.types";
-import { RegisterDto } from "./dto/user.dto";
-import { BadRequestException, Query } from "@nestjs/common";
-import { User } from "./entities/user.entity";
-// import { Response } from "express";
-
+import { Args, Mutation, Resolver, Query, Context } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { RegisterResponse } from './types/user.types';
+import { RegisterDto } from './dto/user.dto';
+import { BadRequestException } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { Response } from 'express';
 
 @Resolver('User')
 // @UseFilters()
-export class usersResolver {
-    constructor(
-        private readonly userService: UsersService
-    ){}
+export class UsersResolver {
+  constructor(private readonly userService: UsersService) {}
 
-    @Mutation(() => RegisterResponse)
-    async register(
-        @Args('registerInput') registerDto: RegisterDto,
-        // @Context() context: { res: Response},
-
-    ): Promise<RegisterResponse> {
-        if(!registerDto.name || !registerDto.email || !registerDto.password) {
-           throw new BadRequestException('Please fill all fields!') ;
-        }
-
-        // const user = await this.userService.register(registerDto, context.res); 
-        const user = await this.userService.register(registerDto); 
-        return {user}; //temp setup
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerInput') registerDto: RegisterDto,
+    @Context() context: { res: Response },
+  ): Promise<RegisterResponse> {
+    if (!registerDto.name || !registerDto.email || !registerDto.password) {
+      throw new BadRequestException('Please fill all fields!');
     }
 
-    @Query(() => [User])
-    async getAllUsers() {
-        return this.userService.getAllUsers(); //temp setup
-    }
+    // const user = await this.userService.register(registerDto, context.res);
+    const user = await this.userService.register(registerDto, context.res);
+    return { user }; //temp setup
+  }
+
+  @Query(() => [User])
+  async getAllUsers() {
+    return this.userService.getAllUsers(); //temp setup
+  }
 }
--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,25 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto, RegisterDto } from './dto/user.dto';
+import { PrismaService } from '../../../prisma/prisma.service';
+import { Response } from 'express';
 
 @Injectable()
 export class UsersService {
   constructor(
     private readonly jwtService: JwtService,
-    // private readonly prisma:
+    private readonly prisma: PrismaService,
     private readonly configService: ConfigService,
   ) {}
 
   //register user service
-  async register(registerDto: RegisterDto) {
+  async register(registerDto: RegisterDto, response: Response) {
     const { name, email, password } = registerDto;
-    const user = {
-      name,
-      email,
-      password,
-    };
-    return user; //temp setup
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        email,
+      }
+    })
+    if(existingUser) {
+      throw new BadRequestException('Email already exists');
+    }
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    });
+
+    return { user, response };
   }
 
   //login user service
@@ -34,14 +47,6 @@ export class UsersService {
 
   //get all users service
   async getAllUsers() {
-    const users = [
-      {
-        id: 1,
-        name: 'Al Capone',
-        email: 'al@nochance.com',
-        password: "123456",
-      },
-    ];
-    return users; //temp setup
+    return this.prisma.user.findMany({});
   }
 }
