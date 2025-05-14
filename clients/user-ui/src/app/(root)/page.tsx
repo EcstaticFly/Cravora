92e25e7e0b6b729f54070db766885982f460b8c8
Suyash Pandey <suyashpandey607@gmail.com>
2025-05-14 23:08:09 +0530

    feat: initialize client-side file structure with Next.js, TypeScript, and Tailwind CSS

---
+++ clients/user-ui/src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import HomePage from '@/src/pages/HomePage'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  )
+}
+
+export default Page
